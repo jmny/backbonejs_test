5911c547e2fd9a60a31b5405ca8bf67338c1f8f0
Louis PERRIER <louis@jmny.eu>
2012-03-20 00:07:00 +0100

    lib and skeleton

---
+++ app.js
@@ -0,0 +1,12 @@
+/* 
+ * TODO :
+ * - slider :       un curseur modifiable qui modifie le modele de la valeur courante
+ *                  et emet un evenement
+ *                  
+ * - logger :       stoque et affiche l'historique des valeurs avec la date de modification
+ * 
+ * - last_value :   affiche la dernière valeur choisi   
+ */
+
+var plop;
+
