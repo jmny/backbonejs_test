3cff2b12106d85602e528f6c404bcce3a11c2dd0
Louis PERRIER <louis@jmny.eu>
2012-05-21 22:29:06 +0200

    wip ajout model

--- app.js
+++ app.js
@@ -9,9 +9,39 @@
 var main = {};
 $(document).ready(function() {
     (function($, main) {
+
+        var Item = Backbone.Model.extend({
+
+            validate:function (attributes){
+                if(!attributes.name) {
+                    return "name must be set!";
+                }
+            },
+
+            initialize : function Doc() {
+                console.log('Doc Constructor');
+
+                this.bind("error", function(model, error){
+                    console.log( error );
+                });
+
+            },
+        });
+
+        var List = Backbone.Collection.extend({
+            model : Item,
+            url: function() {
+                return this.document.url() + '/list';
+            },
+            initialize : function() {
+                console.log('Docs collection Constructor');
+            }
+        });
+
+
         var ListView = Backbone.View.extend({
             el: $('#mylist'),
-
+            model: List,
             initialize: function(){
                 _.bindAll(this, 'render');
 
